3174741b72805965aeb29cb839cab5f42a2c8d67
eunhwa-a11y <ttab136@gmail.com>
2024-11-26 14:54:32 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <div id="app" class="container">
+    <h1>Vue Todo App</h1>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
